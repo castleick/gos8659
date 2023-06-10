4303b1aedec354c6af25d4ed0c28d1793d5dee26
gos8658 <gos8658@naver.com>
2023-06-10 16:34:05 +0900

    app

--- text.py
+++ text.py
@@ -1,10 +1,2 @@
-import sympy
-
-s = sympy.symbols('s')
-G = 100 / ((s + 2) * (s + 3))
-
-H = 1  # 피드백 경로의 전달함수
-
-closed_loop_tf = G * H
-
-closed_loop_tf.simplify()
+import streamlit as st
+print("hello world")
